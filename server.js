c5b0618d96e00449ecaf857664bfb3661747a935
Mohit <me.mohitsharma14@gmail.com>
2024-10-05 17:58:50 +0530

    Try to make a small server

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
-const http = require('http')
-
-var server = http.createServer(function(req,res){
-    res.end("hello ")
+const express = require('express');
+const app = express();
+const port =3000;
+app.get('/', (req, res) => {
+    res.send('Hello World!');
 })
 
-server.listen(3000)
+app.get('/login', (req, res) => {
+    res.send('<h1>Login Page</h1>');
+})
+app.listen(port, () => {
+    console.log(`App listening on port ${port}!`);
+});
